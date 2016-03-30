49ebb2523de33149bb17d87f6fa4b5a8aced62ae
Diego Paiva <diegopaiva.ti@gmail.com>
2016-03-30 20:25:23 -0300

    1 questão do exercício do 2 estagio

---
+++ src/Inventario.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventario{
+
+	private List<Item> itens = new ArrayList<Item>();
+
+	public Inventario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<Item> getItens() {
+		return itens;
+	}
+
+	public void setItens(List<Item> itens) {
+		this.itens = itens;
+	}
+	
+	
+}
