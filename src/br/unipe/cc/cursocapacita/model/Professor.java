41b1640d63a39ce637b3629d5ceac247640d7603
Diego Paiva <diegopaiva.ti@gmail.com>
2016-03-30 20:24:01 -0300

    2 questão

---
+++ src/br/unipe/cc/cursocapacita/model/Professor.java
@@ -0,0 +1,35 @@
+package br.unipe.cc.cursocapacita.model;
+
+public class Professor {
+	
+	
+	private String nomeProfessor;
+	private int matriculaProfessor;
+	private String especialidadeProfessor;
+	public Professor(String nomeProfessor, int matriculaProfessor,
+			String especialidadeProfessor) {
+		this.nomeProfessor = nomeProfessor;
+		this.matriculaProfessor = matriculaProfessor;
+		this.especialidadeProfessor = especialidadeProfessor;
+	}
+	public String getNomeProfessor() {
+		return nomeProfessor;
+	}
+	public void setNomeProfessor(String nomeProfessor) {
+		this.nomeProfessor = nomeProfessor;
+	}
+	public int getMatriculaProfessor() {
+		return matriculaProfessor;
+	}
+	public void setMatriculaProfessor(int matriculaProfessor) {
+		this.matriculaProfessor = matriculaProfessor;
+	}
+	public String getEspecialidadeProfessor() {
+		return especialidadeProfessor;
+	}
+	public void setEspecialidadeProfessor(String especialidadeProfessor) {
+		this.especialidadeProfessor = especialidadeProfessor;
+	}
+	
+	
+}
---
+++ src/br/unipe/cc/cursocapacita/ui/Principal.java
@@ -0,0 +1,22 @@
+package br.unipe.cc.cursocapacita.ui;
+
+import br.unipe.cc.cursocapacita.fachada.CursoCapacitacao;
+import br.unipe.cc.cursocapacita.fachada.UsaAlunos;
+import br.unipe.cc.cursocapacita.model.Professor;
+
+
+public class Principal {
+	private CursoCapacitacao curso;
+
+	public static void main(String[] args) {
+		Principal p = new Principal();
+
+		p.curso = new CursoCapacitacao(001, "Computaçao", new Professor(
+				"Matheus", 122115, "Programador"), new UsaAlunos());
+
+		
+		
+		//Autores: Diego Paiva e Edyara Araujo
+		
+	}
+}
